fb9c4a90f3def1a94357bad7abc7e0aa595be69b
bshearrer <bshearr@outlook.com>
2022-03-12 16:55:59 -0600

    Project Design Finished

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var optionOneButton: UIButton!
+    @IBOutlet weak var optionTwoButton: UIButton!
+    
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story0
+        optionOneButton.setTitle(choice1, for: .normal)
+        optionTwoButton.setTitle(choice2, for: .normal)
     }
 
-
+    @IBAction func choiceClicked(_ sender: UIButton) {
+    }
+    
 }
 
